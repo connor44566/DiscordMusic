f96b6fc51e1bdc9c815198d9cbb0312b4bacf34d
MinnDevelopment <minn69minn@gmail.com>
2016-07-04 17:32:32 +0200

    Added comments/docs to PythonEval

--- src/main/java/minn/music/commands/PythonEval.java
+++ src/main/java/minn/music/commands/PythonEval.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 
 public class PythonEval extends EvalCommand
 {
-	private File f = new File("testing.py");
+	private File f = new File("PewDie.py");
 	private final static SimpleLog LOG = SimpleLog.getLog("PythonEval");
 
 	public PythonEval(MusicBot bot) throws IOException
@@ -30,6 +30,8 @@ public class PythonEval extends EvalCommand
 		}
 		try
 		{
+
+			// Create Python file
 			f.createNewFile();
 			f.deleteOnExit();
 			OutputStream stream = new BufferedOutputStream(new FileOutputStream(f));
@@ -37,21 +39,24 @@ public class PythonEval extends EvalCommand
 			stream.write(event.allArgs.getBytes());
 			stream.close();
 
+			// Start process
 			ProcessBuilder builder = new ProcessBuilder();
 			builder.command("python", f.getName());
 			Process p = builder.start();
 
+			// Create Stream Scanner
 			Scanner sc = new Scanner(p.getInputStream());
 			Scanner scErr = new Scanner(p.getErrorStream());
 
-
+			// Read streams
 			if (sc.hasNext())
 				event.send(read(sc));
 			else if(scErr.hasNext())
 				event.send("ERROR: " + read(scErr));
 			else
-				event.send("Nothing returned.");
+				event.send("✅");
 
+			// Destroy Process
 			p.destroyForcibly();
 			LOG.debug("Process Destroyed");
 		} catch (Exception e)
@@ -60,6 +65,12 @@ public class PythonEval extends EvalCommand
 		}
 	}
 
+	/**
+	 * Reads given {@link java.util.Scanner} until the output reaches 1000 characters or more. Or no next is available.
+	 * @param out
+	 *          A Scanner to an {@link OutputStream}
+	 * @return A String containing the Streams contents.
+	 */
 	private static String read(Scanner out)
 	{
 		assert out != null;
